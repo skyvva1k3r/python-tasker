cf6d9bd298f3edb2bd0b0a70f98a0cc1026c5349
skyLaptop <antargtig@inbox.ru>
2025-09-15 17:01:02 +0300

    deleting system is finished, date fool check is in progress.

--- main.py
+++ main.py
@@ -3,7 +3,8 @@ import os
 from datetime import datetime
 
 l = []
-obj = ["name", "description", "expires", "deadline"]
+obj = ["name", "description", "current", "deadline"]
+months = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"]
 
 def menu():
     print("\nPrint '1' to list all tasks.\nPrint '2' to add task.\nPrint '3' to delete task.\nPrint '4' to kill tasker.\n")
@@ -22,9 +23,17 @@ def menu():
             return 0
 
 def list_task():
-    print("List of your tasks")
+    if len(l) == 0:
+        print("You have no tasks for now.")
+    else:
+        print("List of your tasks:\n")
     for i in range(len(l)):
         print("\n", *l[i], sep = "\n")
+
+def dateFoolCheck(ddl, cur):
+    ddlList, curList = ddl.split(), cur.split()
+    print(ddlList, curList)
+
 
 def add_task():
     temp = []
@@ -35,22 +44,29 @@ def add_task():
                 temp.append(input("\nEnter the name of task:\n"))
             case 1:
                 temp.append(input("Enter the desc of task:\n"))
-            case 2:
+            case 3:
                 tempStr = input("Enter a date (YYYY-MM-DD HH:MM):\n")
                 try: 
                     tempStr = datetime.strptime(tempStr, "%Y-%m-%d %H:%M")
                 except ValueError:
                     print("\nWrong time format, try again.")
                     return 0
+                if dateFoolCheck(tempStr, (time.ctime(time.mktime(time.gmtime())+10800))) == False:
+                    return 0
                 tempStr = datetime.strftime(tempStr, "%c")
                 temp.append(tempStr)
-            case 3:
+            case 2:
                 temp.append(time.ctime(time.mktime(time.gmtime())+10800))
     l.append(temp)
 
 def delete_task():
-    print("\nEnter the name of the task you want to delete:\n")
+    print("\nEnter the name of the task you want to delete:\n(Attention, will be deleted first found task with entered name)")
+    name = input()
+    for i in range(len(l)):
+        if name in l[i]:
+            l.pop(i)
+            break
     
-
+    
 while True:
     menu()
